3ab815ab7ad6ad7b0e204966947efd18a155c951
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-03 01:21:36 -0700

    Clean up gpio to satisfy errors

--- src/gpio.h
+++ src/gpio.h
@@ -10,7 +10,7 @@ struct gpio {
 
 // GPIO pin mode
 enum {
-  GPIO_INPUT,
+  GPIO_INPUT = 0,
   GPIO_OUTPUT_10MHZ,
   GPIO_OUTPUT_2MHZ,
   GPIO_OUTPUT_50MHZ
@@ -18,35 +18,48 @@ enum {
 
 // GPIO pin configuration
 enum {
-  GPIO_IN_ANALOG,
+  GPIO_IN_ANALOG = 0,
   GPIO_IN_FLOATING,
   GPIO_IN_PULL_UP_DOWN
 };
 enum {
-  GPIO_OUT_PUSH_PULL,
+  GPIO_OUT_PUSH_PULL = 0,
   GPIO_OUT_OPEN_DRAIN,
   GPIO_OUT_ALT_PUSH_PULL,
   GPIO_OUT_ALT_OPEN_DRAIN
 };
 
-#define GPIO(port) ((struct gpio *) 0x40010800 + (0x400 * (port - 'A')))
-#define PIN(port, num) (((port) << 8) | num) // uint16_t
+// GPIO State
+enum {
+  GPIO_OFF = 0,
+  GPIO_ON
+};
+
+#define GPIO(port) ((struct gpio *) (0x40010800 + (0x400 * ((port) - 'A'))))
+#define PIN(port, num) (((port) << 8) | (num)) // uint16_t
 #define PIN_PORT(pin) ((pin) >> 8)
-#define PIN_NUM(pin) ((pin) & 255)
+#define PIN_NUM(pin) (uint8_t)((pin) & 255)
 
 
 static inline void gpio_set(uint16_t pin, uint8_t mode, uint8_t configuration) {
-  gpio *gpio = GPIO(PIN_PORT(pin));
+  struct gpio *gpio = GPIO(PIN_PORT(pin));
   uint8_t pin_num = PIN_NUM(pin);
 
   if(pin_num < 8) {
-    gpio->CRL &= ~(7 << (pin_num * 4));
-    gpio->CRL |= (((configuration << 2) | mode) << (pin_num * 4));
+    gpio->CRL &= ~(15U << (pin_num * 4));
+    gpio->CRL |= (uint32_t)(((configuration << 2) | mode) << (pin_num * 4));
   }
   else {
-    gpio->CRH &= ~(7 << (pin * 4));
-    gpio->CRH |= (((configuration << 2) | mode) << (pin_num * 4));
+    gpio->CRH &= ~(15U << ((pin_num - 8) * 4));
+    gpio->CRH |= (uint32_t)(((configuration << 2) | mode) << ((pin_num - 8) * 4));
   }
+}
+
+static inline void gpio_write(uint16_t pin, uint8_t state) {
+  struct gpio *gpio = GPIO(PIN_PORT(pin));
+  uint8_t pin_num = PIN_NUM(pin);
+
+  gpio->BSRR = (uint32_t)(1 << pin_num) << (state ? 0 : 16);
 }
 
 
