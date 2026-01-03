192893b47ef0dd4fec6ebce56a407d4a6247f117
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-03 01:25:48 -0700

    Minimal main

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+
+#include "gpio.h"
+#include "rcc.h"
+
+//extern void Reset_Handler();
+
+int main(void) {
+  //for(char i = 'A'; i < 'H'; i++) {
+  //  RCC->APB2ENR |= ((1UL << 2) << (i - 'A'));
+  //  for(int j = 0; j < 16; j++) {
+  //    uint16_t led = PIN(i, j);
+  //    gpio_set(led, GPIO_OUTPUT_10MHZ, GPIO_OUT_PUSH_PULL);
+  //    gpio_write(led, GPIO_ON);
+  //  }
+  //  
+  //  
+  //}
+  uint16_t led = PIN('B', 2);
+  RCC->APB2ENR |= (1 << 3);
+  gpio_set(led, GPIO_OUTPUT_10MHZ, GPIO_OUT_PUSH_PULL);
+
+
+  for(;;) {
+    gpio_write(led, GPIO_OFF);
+    (void) 0;
+  };
+  return 0;
+}
+
+// Startup code
+__attribute__((naked, noreturn)) void _reset(void) {
+  // memset .bss to zero, and copy .data section to RAM region
+  extern long _sbss, _ebss, _sdata, _edata, _sidata;
+  for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
+  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+
+  main();             // Call main()
+  for (;;) (void) 0;  // Infinite loop in the case if main() returns
+}
+
+extern void _estack(void);
+
+__attribute__((section(".isr_vector"))) void (*const tab[16 + 91])(void) = {
+  _estack, _reset
+};
