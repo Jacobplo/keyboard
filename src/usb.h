5cc29537e9b706408937af3bd3e95dcc208de438
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-12 08:52:57 -0700

    Remove board-specific defines from MCU usb.h

--- src/usb.h
+++ src/usb.h
@@ -4,9 +4,6 @@
 #include "rcc.h"
 #include "gpio.h"
 
-#define USB_DN PIN('A', 11)
-#define USB_DP PIN('A', 12)
-
 static inline void usb_init(void) {
   // Enable USB clock. Clock must be initialized properly first.
   RCC->APB1ENR |= (1 << 23);
