ea80dd5643d6bc7734edbaba310047c032568736
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-13 16:49:09 -0700

    Remove unused rcc.h enum

--- src/rcc.h
+++ src/rcc.h
@@ -7,19 +7,6 @@ struct rcc {
   volatile uint32_t CR, CFGR, CIR, APB2RSTR, APB1RSTR, AHBENR, APB2ENR, APB1ENR, BDCR, CSR;
 };
 
-enum {
-  RCC_CR,
-  RCC_CFGR,
-  RCC_CIR,
-  RCC_APB2RSTR,
-  RCC_APB1RSTR,
-  RCC_AHBENR,
-  RCC_APB2ENR,
-  RCC_APB1ENR,
-  RCC_BDCR,
-  RCC_CSR
-};
-
 #define RCC ((struct rcc *) 0x40021000)
 
 #endif
