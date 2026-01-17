90b07b4d0a5c76d9f320f926abd18159794fd389
Jacob Plourde <jacob_plourde@hotmail.com>
2026-01-17 15:21:25 -0700

    Add function to get current systick ticks

--- src/systick.c
+++ src/systick.c
@@ -10,3 +10,7 @@ void delay_ticks(uint32_t ticks) {
   uint32_t start = s_ticks;
   while(s_ticks < start + ticks) (void) 0;
 }
+
+uint32_t get_ticks(void) {
+  return s_ticks;
+}
--- src/systick.h
+++ src/systick.h
@@ -6,11 +6,14 @@
 #include "stm32f103xb.h"
 #include "system_stm32f1xx.h"
 
+
 static inline void systick_init(void) {
   SystemCoreClockUpdate();
   SysTick_Config(SystemCoreClock / 1000);
 }
 
 void delay_ticks(uint32_t ticks);
+
+uint32_t get_ticks(void);
 
 #endif
